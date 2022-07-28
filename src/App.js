961c4490d8b06dab85d6939bd6fedda53a249083
Uchchash346 <mru416@gmail.com>
2022-07-28 06:57:29 +0600

    Project Initialize

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import React from 'react';
 import { FiSettings } from 'react-icons/fi';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
 const App = () => {
